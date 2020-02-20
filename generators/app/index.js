81c23030d926fe61a25db8ea039ba0297ec20e01
Chris Blackmon <blackmon@monkee-boy.com>
2020-02-20 14:11:01 -0600

    Able to create package.json from template.

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,8 @@
 var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var remote = require('yeoman-remote');
+var s = require("underscore.string");
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -12,64 +14,85 @@ module.exports = class extends Generator {
     // Next, add your custom code
     this.option('babel'); // This method adds support for a `--babel` flag
   }
-  method1() {
-    this.log('method 1 just ran');
+    async prompting() {
+      const self = this;
+
+      self.log(['\n',
+        chalk.yellow(' ##     ##    #####    ##    ##   ##   ##  #######  #######         #####      #####   ##    ##'),
+        chalk.yellow('###    ###   #######   ###   ##   ##  ##   #######  #######         ######    #######  ##    ##'),
+        chalk.yellow('####   ###  ##     ##  ###   ##   ## ##    ##       ##              ##  ##   ##     ## ##    ##'),
+        chalk.yellow('####  ####  ##     ##  ####  ##   ####     ##       ##              ##  ##   ##     ##  ##  ##'),
+        chalk.yellow('## #### ##  ##     ##  ## ## ##   ####     ######   ######   ####   ######   ##     ##   ####'),
+        chalk.yellow('##  ##  ##  ##     ##  ## ## ##   #####    ######   ######   ####   ######   ##     ##    ##'),
+        chalk.yellow('##  ##  ##  ##     ##  ##  ####   ## ###   ##       ##              ##   ##  ##     ##    ##'),
+        chalk.yellow('##      ##  ##     ##  ##   ###   ##  ##   ##       ##              ##   ##  ##     ##    ##'),
+        chalk.yellow('##      ##   #######   ##   ###   ##   ##  #######  #######         #######   #######     ##'),
+        chalk.yellow('##      ##    #####    ##    ##   ##   ### #######  #######         ######     #####      ##')].join('\n')
+      );
+
+    // Have Yeoman greet the user.
+    self.log(yosay(
+      'Welcome to the ' + chalk.green('MBoy Gulp') + ' v1.0 generator!'
+    ));
+
+    self.log('I am going to generate the default Monkee-Boy gulp setup. Reference https://github.com/Monkee-Boy/gulpfile for more details.\n\nFirst a couple of quick questions.\n');
+
+    // remote('Monkee-Boy', 'gulpfile', function (err, remote) {
+    //   remote.directory('.', '.');
+    // });
+
+    // var prompts = [
+    //   {
+    //     name: 'projectName',
+    //     message: 'What would you like to call this project (typically client name)?',
+    //     default: 'Project Name'
+    //   }, {
+    //     name: 'repoUrl',
+    //     message: 'What is the git repo url for this project?',
+    //     default: 'git@bitbucket.org:monkeeboy/PROJECTREPONAME.git'
+    //   }
+    // ];
+
+    self.answers = await self.prompt([{
+      type: 'input',
+      name: 'projectName',
+      message: 'What would you like to call this project (typically client name)?',
+    }, {
+      type: 'input',
+      name: 'repoUrl',
+      message: 'What is the git repo url for this project?',
+      default: 'git@bitbucket.org:monkeeboy/PROJECTREPONAME.git'
+    }]);
+
+    // return self.prompt(prompts).then(function (props) {
+    //   self.props = props;
+    //   self.props.projectName = s.slugify(props.projectName);
+    // }.bind(self));
+
   }
 
-  method2() {
-    this.log('method 2 just ran');
+  writing() {
+    const self = this;
+    
+    self.fs.copyTpl(
+      self.templatePath('package.json'),
+      self.destinationPath('package.json'),
+      { 
+        projectName: self.answers.projectName, 
+        repoUrl: self.answers.repoUrl
+      }
+    );
   }
+
+  install() {
+    this.log('\nYour gulp files and structure are ready.\n' + chalk.green('May the code be with you.'));
+  }
+
+  // method1() {
+  //   this.log('method 1 just ran');
+  // }
+
+  // method2() {
+  //   this.log('method 2 just ran');
+  // }
 }
-
-// module.exports = yeoman.Base.extend({
-//   prompting: function () {
-//     this.log(['\n',
-//       chalk.yellow(' ##     ##    #####    ##    ##   ##   ##  #######  #######         #####      #####   ##    ##'),
-//       chalk.yellow('###    ###   #######   ###   ##   ##  ##   #######  #######         ######    #######  ##    ##'),
-//       chalk.yellow('####   ###  ##     ##  ###   ##   ## ##    ##       ##              ##  ##   ##     ## ##    ##'),
-//       chalk.yellow('####  ####  ##     ##  ####  ##   ####     ##       ##              ##  ##   ##     ##  ##  ##'),
-//       chalk.yellow('## #### ##  ##     ##  ## ## ##   ####     ######   ######   ####   ######   ##     ##   ####'),
-//       chalk.yellow('##  ##  ##  ##     ##  ## ## ##   #####    ######   ######   ####   ######   ##     ##    ##'),
-//       chalk.yellow('##  ##  ##  ##     ##  ##  ####   ## ###   ##       ##              ##   ##  ##     ##    ##'),
-//       chalk.yellow('##      ##  ##     ##  ##   ###   ##  ##   ##       ##              ##   ##  ##     ##    ##'),
-//       chalk.yellow('##      ##   #######   ##   ###   ##   ##  #######  #######         #######   #######     ##'),
-//       chalk.yellow('##      ##    #####    ##    ##   ##   ### #######  #######         ######     #####      ##')].join('\n')
-//     );
-
-//     // Have Yeoman greet the user.
-//     this.log(yosay(
-//       'Welcome to the ' + chalk.green('MBoy Gulp') + ' v1.0 generator!'
-//     ));
-
-//     this.log('I am going to generate the default Monkee-Boy gulp setup. Reference https://github.com/Monkee-Boy/gulpfile for more details.\n\nFirst a couple of quick questions.\n');
-
-//     this.remote('Monkee-Boy', 'gulpfile', function (err, remote) {
-//       remote.directory('.', '.');
-//     });
-
-//     var prompts = [
-//       {
-//         name: 'projectName',
-//         message: 'What would you like to call this project (typically client name)?',
-//         default: 'Project Name'
-//       }, {
-//         name: 'repoUrl',
-//         message: 'What is the git repo url for this project?',
-//         default: 'git@bitbucket.org:monkeeboy/PROJECTREPONAME.git'
-//       }
-//     ];
-
-//     return this.prompt(prompts).then(function (props) {
-//       this.props = props;
-//       this.props.projectName = string.slugify(props.projectName);
-//     }.bind(this));
-//   },
-
-//   writing: function () {
-//     // this.template('package.json', 'package.json');
-//   },
-
-//   install: function () {
-//     this.log('\nYour gulp files and structure are ready.\n' + chalk.green('May the code be with you.'));
-//   }
-// });
